d9f92e8efbe473044f7b9f98751a494e9d1786d8
Richard Scassellati <rn.scassellati@setonhill.edu>
2020-05-01 23:26:12 -0400

    Initial Commit

---
+++ ScassellatiHomework10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScassellatiHomework10
+//
+//  Created by Richard Scassellati on 5/1/20.
+//  Copyright © 2020 Smecko Geck Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ScassellatiHomework10Tests/ScassellatiHomework10Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ScassellatiHomework10Tests.swift
+//  ScassellatiHomework10Tests
+//
+//  Created by Richard Scassellati on 5/1/20.
+//  Copyright © 2020 Smecko Geck Productions. All rights reserved.
+//
+
+import XCTest
+@testable import ScassellatiHomework10
+
+class ScassellatiHomework10Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
